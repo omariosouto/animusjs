c4351126afe5ca34966a3040a34fdfc9622855cd
Mario Souto <Mario Souto>
2017-01-11 21:46:35 -0200

    Initializing core

---
+++ src/animus.js
@@ -0,0 +1,20 @@
+(function(world){
+  'use strict';
+
+
+  /**
+   * [animusjs description]
+   * @return {[Object]}
+   */
+  world.animusjs = function(){
+
+    return {
+      core : {
+        events : {},
+      }
+    };
+
+  }();
+
+
+}(this));

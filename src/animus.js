f40109f1fa747b3e95192e892a046e898ac2f339
Mario Souto <mariosoutoskn@gmail.com>
2017-02-14 15:15:29 -0200

    Anim-in-out finished, now I need to refactor

--- src/animus.js
+++ src/animus.js
@@ -10,59 +10,51 @@
 
     return {
       core : {
-        events : {
-          scroll : {
-            direction: '',
-            current: 0,
-            previous: 0,
-            getScroll: function getScroll(e) {
-              var scroll = animusjs.core.events.scroll;
-              var animIn = animusjs.core.events.animIn;
-              scroll.current = window.pageYOffset;
+        scrollChecker: function scrollChecker() {
 
-              if(scroll.current > scroll.previous) {
-                scroll.direction = 'down';
-                console.log( animIn.event() );
-              } else {
-                scroll.direction = 'up';
-              }
-
-              scroll.previous = window.pageYOffset;
-            },
-            init: function() {
-              window.addEventListener('scroll', this.getScroll, false);
-              console.log(this);
-            }
-          },
-          animIn : {
-            event: function event() {
-              var scroll = animusjs.core.events.scroll;
-              var animElements = document.querySelectorAll('[anim-in]')
-              function isElementInView (element, fullyInView) {
-                  var pageTop = scroll.current;
-                  var pageBottom = pageTop + window.innerHeight;
-                  var elementTop = element.offsetTop;
-                  var elementBottom = elementTop + element.style.height;
-
-                  if (fullyInView === true) {
-                      return ((pageTop < elementTop) && (pageBottom > elementBottom));
-                  } else {
-                      return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
-                  }
-              }
-
-              Array.prototype.forEach.call(animElements, function(element, index) {
-
-                if (isElementInView(element, false) && !element.classList.contains('anm-animated') ) {
-                  console.log(element);
-                  console.log('Add Class: ', element.classList.add('anm-animated'));
-                }
-
-              });
-
-            }
-          }
         },
+        animIn: function animIn() {
+
+        },
+        animOut: function animOut() {
+
+        },
+        animInOut: function animInOut() {
+          console.log('ANIM IN FUNCTION');
+          var animInOutElements  = document.querySelectorAll('[anim-in-out]');
+          var currentListOfElements = animInOutElements;
+
+
+              console.log(currentListOfElements);
+
+          function scrollChecker(e, currentListOfElements) {
+
+            Array.prototype.forEach.call(animInOutElements, function(sliderImage, index) {
+                // half way through the image
+                var slideInAt = (window.scrollY + window.innerHeight) - sliderImage.offsetHeight / 2;
+                // bottom of the image
+                var imageBottom = sliderImage.offsetTop + sliderImage.offsetHeight;
+                var isHalfShown = slideInAt > sliderImage.offsetTop;
+                var isNotScrolledPast = window.scrollY < imageBottom;
+
+                if (isHalfShown && isNotScrolledPast) {
+                  sliderImage.classList.add('teste');
+                } else {
+                  sliderImage.classList.remove('teste');
+                }
+            });
+
+          }
+
+          window.addEventListener('scroll', scrollChecker);
+
+        }
+      },
+      init: function() {
+
+        this.core.animInOut();
+
+        console.log('[ANIMUSJS INITIALIZED]');
       }
     };
 
@@ -73,28 +65,4 @@
 
 }(this));
 
-window.animusjs.core.events.scroll.init();
-
-
-    // - In view element
-    // isElementInView: function (element, fullyInView) {
-    //     var pageTop = $(window).scrollTop();
-    //     var pageBottom = pageTop + $(window).height();
-    //     var elementTop = $(element).offset().top;
-    //     var elementBottom = elementTop + $(element).height();
-
-    //     if (fullyInView === true) {
-    //         return ((pageTop < elementTop) && (pageBottom > elementBottom));
-    //     } else {
-    //         return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
-    //     }
-    // }
-
-
-    // var isElementInView = Utils.isElementInView($('#flyout-left-container'), false);
-
-    // if (isElementInView) {
-    //     console.log('in view');
-    // } else {
-    //     console.log('out of view');
-    // }
+window.animusjs.init();

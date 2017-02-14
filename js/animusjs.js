6cd07aaeb74f56970f4b9ac1460c22d24f55ad27
Mario Souto <mariosoutoskn@gmail.com>
2017-02-14 17:14:08 -0200

    Update 2017-02-14T19:14:04.108Z

--- js/animusjs.js
+++ js/animusjs.js
@@ -1,1 +1,144 @@
-function active(t,n){"animate"===n.status&&t.classList.add("active"),"reverse"===n.status&&t.classList.remove("active")}!function(t){"use strict";t.animusjs=function(){return{core:{scrollChecker:function(){},animIn:function(){function t(){Array.prototype.forEach.call(i,function(t,i){var e=t.getAttribute(n),s=t.getAttribute("anim-type"),r=window.scrollY+window.innerHeight-t.offsetHeight/2,c=t.offsetTop+t.offsetHeight,a=r>t.offsetTop,u=window.scrollY<c;a&&u&&("function"===s?window[e](t,o):t.classList.add(e))})}var n="anim-in",i=document.querySelectorAll("[anim-in]"),o={};o.status="animate",window.addEventListener("scroll",t)},animOut:function(){function t(){Array.prototype.forEach.call(i,function(t,i){var e=t.getAttribute(n),s=t.getAttribute("anim-type"),r=window.scrollY+window.innerHeight-t.offsetHeight/2,c=t.offsetTop+t.offsetHeight,a=r>t.offsetTop,u=window.scrollY<c;a&&u||("function"===s?window[e](t,o):t.classList.remove(e))})}var n="anim-out",i=document.querySelectorAll("[anim-out]"),o={};o.status="reverse",window.addEventListener("scroll",t)},animInOut:function(){function t(){Array.prototype.forEach.call(i,function(t,i){var e=t.getAttribute(n),s=t.getAttribute("anim-type"),r=window.scrollY+window.innerHeight-t.offsetHeight/2,c=t.offsetTop+t.offsetHeight,a=r>t.offsetTop,u=window.scrollY<c;a&&u?"function"===s?window[e](t,o):t.classList.add(e):"function"===s?window[e](t,o):t.classList.remove(e)})}var n="anim-in-out",i=document.querySelectorAll("[anim-in-out]"),o={};window.addEventListener("scroll",t)}},init:function(){this.core.animIn(),this.core.animOut(),this.core.animInOut(),console.log("[AnimusJS INITIALIZED]")}}}()}(this),window.animusjs.init();
+(function(world){
+  'use strict';
+
+
+  /**
+   * [animusjs description]
+   * @return {[Object]}
+   */
+  world.animusjs = (function(){
+
+    return {
+      core : {
+        scrollChecker: function scrollChecker() {
+
+        },
+        animIn: function animIn() {
+
+          //console.log('[ANIM IN FUNCTION]');
+          var animState = 'anim-in';
+          var animInElements  = document.querySelectorAll('[anim-in]');
+          var options = {};
+          options.status = 'animate';
+
+          function animInScrollChecker() {
+
+            Array.prototype.forEach.call(animInElements, function(currElement, index) {
+                var attribute = currElement.getAttribute(animState);
+                var animType  = currElement.getAttribute('anim-type');
+
+                // half way through the element
+                var slideInAt = (window.scrollY + window.innerHeight) - currElement.offsetHeight / 2;
+                // bottom of the element
+                var elementBottom = currElement.offsetTop + currElement.offsetHeight;
+                var isHalfShown = slideInAt > currElement.offsetTop;
+                var isNotScrolledPast = window.scrollY < elementBottom;
+
+                if (isHalfShown && isNotScrolledPast) {
+                  if(animType === 'function') {
+                    window[attribute](currElement, options);
+                  } else {
+                    currElement.classList.add(attribute);
+                  }
+                }
+            });
+
+          }
+
+          window.addEventListener('scroll', animInScrollChecker);
+
+        },
+        animOut: function animOut() {
+
+          //console.log('[ANIM IN FUNCTION]');
+          var animState = 'anim-out';
+          var animOutElements  = document.querySelectorAll('[anim-out]');
+          var options = {};
+
+          options.status = 'reverse';
+
+          function animOutScrollChecker() {
+
+            Array.prototype.forEach.call(animOutElements, function(currElement, index) {
+                var attribute = currElement.getAttribute(animState);
+                var animType  = currElement.getAttribute('anim-type');
+
+                // half way through the element
+                var slideInAt = (window.scrollY + window.innerHeight) - currElement.offsetHeight / 2;
+                // bottom of the element
+                var elementBottom = currElement.offsetTop + currElement.offsetHeight;
+                var isHalfShown = slideInAt > currElement.offsetTop;
+                var isNotScrolledPast = window.scrollY < elementBottom;
+
+                if (isHalfShown && isNotScrolledPast) {
+                  // teste
+                  } else {
+                  if(animType === 'function') {
+                    window[attribute](currElement, options);
+                  } else {
+                    currElement.classList.remove(attribute);
+                  }
+                }
+            });
+
+          }
+
+          window.addEventListener('scroll', animOutScrollChecker);
+
+        },
+        animInOut: function animInOut() {
+          //console.log('[ANIM IN FUNCTION]');
+          var animState = 'anim-in-out';
+          var animInOutElements  = document.querySelectorAll('[anim-in-out]');
+          var options = {};
+
+          function animInOutScrollChecker() {
+
+            Array.prototype.forEach.call(animInOutElements, function(currElement, index) {
+                var attribute = currElement.getAttribute(animState);
+                var animType  = currElement.getAttribute('anim-type');
+
+                // half way through the element
+                var slideInAt = (window.scrollY + window.innerHeight) - currElement.offsetHeight / 2;
+                // bottom of the element
+                var elementBottom = currElement.offsetTop + currElement.offsetHeight;
+                var isHalfShown = slideInAt > currElement.offsetTop;
+                var isNotScrolledPast = window.scrollY < elementBottom;
+
+                if (isHalfShown && isNotScrolledPast) {
+                  if(animType === 'function') {
+                    window[attribute](currElement, options);
+                  } else {
+                    currElement.classList.add(attribute);
+                  }
+                } else {
+                  if(animType === 'function') {
+                    window[attribute](currElement, options);
+                  } else {
+                    currElement.classList.remove(attribute);
+                  }
+                }
+            });
+
+          }
+
+          window.addEventListener('scroll', animInOutScrollChecker);
+
+        }
+      },
+      init: function() {
+
+        this.core.animIn();
+        this.core.animOut();
+        this.core.animInOut();
+
+        console.log('[AnimusJS INITIALIZED]');
+      }
+    };
+
+
+  }());
+
+
+
+}(this));
